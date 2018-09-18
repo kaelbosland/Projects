9af06d8c3955bb176d569cea94678b738326bf7c
boslandk <boslandk@ads.mcmaster.ca>
2018-09-18 11:52:42 -0400

    This is our LifeStyle windows application, first commit

---
+++ Lifestyle/Lifestyle/UI/LoginTile.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Lifestyle.UI
+{
+    partial class LoginTile
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginTile));
+            this.SuspendLayout();
+            // 
+            // LoginTile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "LoginTile";
+            this.Size = new System.Drawing.Size(182, 166);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
